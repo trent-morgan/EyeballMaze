a3041a7139a129a33b997ee1d1be4676e47c7b20
Trent Morgan <tpm0073@arastudent.ac.nz>
2026-03-17 11:23:38 +1300

    Fixed shape, commented out Game

--- src/nz/ac/ara/tpm/eyeballmaze/model/Game.java
+++ src/nz/ac/ara/tpm/eyeballmaze/model/Game.java
@@ -1,25 +1,26 @@
 package nz.ac.ara.tpm.eyeballmaze.model;
 
-public class Game implements ILevelHolder, IGoalHolder, IEyeballHolder, IMoving {
+//implements ILevelHolder, IGoalHolder, ISquareHolder, IEyeballHolder, IMoving
+public class Game {
 
-    private final List<Square[][]> levels = new ArrayList<>();
-
-    private final LevelHolder levelHolder;
-    private final GoalHolder goalHolder;
-    private final SquareHolder squareHolder;
-    private final EyeballHolder eyeballHolder;
-    private final Moving moving;
+//    private final List<Square[][]> levels = new ArrayList<>();
+//
+//    private final LevelHolder levelHolder;
+//    private final GoalHolder goalHolder;
+//    private final SquareHolder squareHolder;
+//    private final EyeballHolder eyeballHolder;
+//    private final Moving moving;
 
     public static void main(String[] args) {
-        this.levelHolder = new LevelHolder();
-        this.goalHolder = new GoalHolder();
-        this.squareHolder = new SquareHolder();
-        this.eyeballHolder = new EyeballHolder();
-        this.moving = new Moving();
+//        this.levelHolder = new LevelHolder();
+//        this.goalHolder = new GoalHolder();
+//        this.squareHolder = new SquareHolder();
+//        this.eyeballHolder = new EyeballHolder();
+//        this.moving = new Moving();
 	}
 
-    public void addLevel(int height, int width) {
-        this.levelHolder.addLevel(height, width);
-    }
+//    public void addLevel(int height, int width) {
+//        this.levelHolder.addLevel(height, width);
+//    }
 
 }
--- src/nz/ac/ara/tpm/eyeballmaze/model/Shape.java
+++ src/nz/ac/ara/tpm/eyeballmaze/model/Shape.java
@@ -3,7 +3,8 @@ package nz.ac.ara.tpm.eyeballmaze.model;
 public enum Shape {
 	DIAMOND, 
 	CROSS, 
-	STAR, F
-	LOWER, BLANK, 
+	STAR, 
+	LOWER, 
+	BLANK, 
 	LIGHTNING 
 }
